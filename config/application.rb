f881f068d4c8245796dadcbbab62a41dfe658b77
sfigart <sfigart@gmail.com>
2012-11-18 21:31:42 -1000

    Assets Precompile

--- config/application.rb
+++ config/application.rb
@@ -66,5 +66,8 @@ module Palmv
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Heroku requires this to be false
+    config.assets.initialize_on_precompile=false
   end
 end
