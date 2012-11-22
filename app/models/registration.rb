1393a7c8f73a49589c93ff35e4f674c79734116e
sfigart <sfigart@gmail.com>
2012-11-22 12:05:02 -1000

    Added model validations. Touched up image logo. Removed browsershots

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -4,24 +4,24 @@ class Registration < ActiveRecord::Base
   serialize :practice_days
   attr_encrypted :birth_date, :key => 'something secret!'
   
-  # validates :player_last_name, :player_first_name,
-  #             :father_last_name, :father_first_name,
-  #             :mother_last_name, :mother_first_name,
-  #             :address_line_1, :city, :state,
-  #             :home_phone,
-  #             :father_cell_phone, :father_work_phone,
-  #             :mother_cell_phone, :mother_work_phone,
-  #             :email_address, :school, :grade,
-  #             :age, :gender,
-  #             :shirt_size,
-  #             :parent_name_for_agreement,
-  #             :volunteer_type,
-  #             :presence => true
+  validates :player_last_name, :player_first_name,
+                :father_last_name, :father_first_name,
+                :mother_last_name, :mother_first_name,
+                :address_line_1, :city, :state,
+                :home_phone,
+                :father_cell_phone, :father_work_phone,
+                :mother_cell_phone, :mother_work_phone,
+                :email_address, :school, :grade,
+                :age, :gender,
+                :shirt_size,
+                :parent_name_for_agreement,
+                :volunteer_type,
+                :presence => true
   validates_date :birth_date
-  # validates :zip_code,      :presence => true, :numericality => { :only_integer => true }
-  # validates :email_address, :presence => true, :email => true
-  # validates :has_medical_insurance, :inclusion => {:in => [true, false], :message => "can't be blank"}
-  # validates :medical_insurance_name, :presence => true, :if => :medical_insurance_name_required?
+  validates :zip_code,      :presence => true, :numericality => { :only_integer => true }
+  validates :email_address, :presence => true, :email => true
+  validates :has_medical_insurance, :inclusion => {:in => [true, false], :message => "can't be blank"}
+  validates :medical_insurance_name, :presence => true, :if => :medical_insurance_name_required?
 
   def medical_insurance_name_required?
     has_medical_insurance
