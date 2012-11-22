afcc333daaf510810b2ee48f793ae21df543b25c
sfigart <sfigart@gmail.com>
2012-11-22 04:23:19 -1000

    Enabled setting for heroku

--- config/application.rb
+++ config/application.rb
@@ -68,6 +68,6 @@ module PalMoanalua
     config.assets.version = '1.0'
     
     # Heroku requires this to be false
-    #config.assets.initialize_on_precompile=false
+    config.assets.initialize_on_precompile=false
   end
 end
