b696cbacaa3b00f36b81595e980eb0d352755cc1
Code For Good <sfigart@gmail.com>
2015-12-11 17:30:51 -1000

    Updated to use sendgrid for production

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -3,6 +3,9 @@ class RegistrationMailer < ActionMailer::Base
   
   def registration_complete(registration)
     @registration = registration
-    mail(:to => registration.email_address, :subject => "PAL MAC Volleyball Registration Received")
+    mail(:to => registration.email_address,
+    	   :subject => "PAL MAC Volleyball Registration Received",
+         :cc => "palmoanalua@gmail.com"
+    	  )
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,13 +70,13 @@ PalMoanalua::Application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.sendgrid.net",
     port: 587,
-    domain: "palmoanalua.herokuapp.com",
     authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
+    domain: "palmoanalua.herokuapp.com",
+    enable_starttls_auto: true
   }
 
   # Log the query plan for queries taking more than this (works
